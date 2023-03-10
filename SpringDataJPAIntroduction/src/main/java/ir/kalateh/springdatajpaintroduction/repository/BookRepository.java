9c22efa3e5252ed46e87388a7f25eec2607a3809
h.kalateh <h.kalateh72@gmail.com>
2023-03-11 01:24:47 +0330

    create Book model and repo. closes #1

---
+++ SpringDataJPAIntroduction/src/main/java/ir/kalateh/springdatajpaintroduction/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package ir.kalateh.springdatajpaintroduction.repository;
+
+import ir.kalateh.springdatajpaintroduction.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book,Long> {
+}
